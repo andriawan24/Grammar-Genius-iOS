325096b7c496d84ac2c6b822c94b13c62ebe94db
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-10-22 22:14:56 +0700

    Initial Commit

---
+++ Grammar Genius/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Grammar Genius
+//
+//  Created by Naufal Fawwaz Andriawan on 22/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Grammar Genius/Grammar_GeniusApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grammar_GeniusApp.swift
+//  Grammar Genius
+//
+//  Created by Naufal Fawwaz Andriawan on 22/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Grammar_GeniusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
