d122af10880e85383fbb14a4f6ce52fb88eb3ebb
Naufal Fawwaz Andriawan <fawaznaufal23@gmail.com>
2024-10-27 22:31:47 +0700

    feature(localization): Create string collections

---
+++ Grammar Genius/Utils/Strings.swift
@@ -0,0 +1,15 @@
+//
+//  Strings.swift
+//  Grammar Genius
+//
+//  Created by Naufal Fawwaz Andriawan on 26/10/24.
+//
+
+class Strings {
+    static let labelGrammarOptions = String(localized: "label_grammar_option")
+    static let labelFix = String(localized: "label_fix")
+    static let labelCheck = String(localized: "label_check")
+    static let labelInput = String(localized: "label_input")
+    static let labelResult = String(localized: "label_result")
+    static let hintInput = String(localized: "hint_input")
+}
